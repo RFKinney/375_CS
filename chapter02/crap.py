2f834c9d05774eddf06cb80c45cd4221d42b6532
rkinney19 <rkinney19@hanoverstudents.org>
2017-10-24 11:30:31 -0400

    tracked winnings, validated input

--- chapter02/crap.py
+++ chapter02/crap.py
@@ -11,25 +11,35 @@
 # A win pays whatever amount was bet. A loss takes the amount bet. The user can continue to play until he/she is out of
 # money.
 #
+# by Mr. Ciccolo
 
 import random
 
 
 def main():
     display_welcome()
+    global winnings, bet
+    winnings = 100
+    bet = 0
 
     play_again = True
     while play_again:
+        bet = place_bet()
+
         total = roll_dice()
         if total == 7 or total == 11:
             print("You win!")
+            winnings = winnings + bet
+            print("You now have $", winnings, sep="")
         elif total == 2 or total == 3 or total == 12:
             print("You lose!")
+            winnings = winnings - bet
+            print("You now have $", winnings, sep="")
         else:
             re_roll(total)
 
         print() # Blank line for spacing
-        play_again = (input("Press enter to play another round or type 'N' to quit ") == '')
+        play_again = (input("Press enter to play another round or type 'N' to quit ") == "")
         clear_screen()
 
 
@@ -45,6 +55,19 @@ def display_welcome():
     print("$                                              $")
     print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
     print()
+
+def place_bet():
+    raw_bet = eval(input("You have $%s. What would you like to bet? ($5-$%s)" % (winnings, winnings)))
+
+    #here we need to validate raw_bet
+    if raw_bet < 5:
+        print("The minimum is $5 so that's what we will use.")
+        raw_bet = 5
+    elif raw_bet > winnings:
+        print("The maximum bet is your", winnings, "that is what we will use.")
+        raw_bet = winnings
+
+    return raw_bet
 
 
 def roll_dice():
@@ -64,6 +87,8 @@ def roll_dice():
 
 
 def re_roll(point):
+    global winnings, bet
+
     print("You have to keep rolling until you get another", point)
     print() # Blank line for spacing
 
@@ -73,8 +98,12 @@ def re_roll(point):
 
     if total == point:
         print("You win!")
+        winnings = winnings + bet
+        print("You now have $", winnings, sep="")
     else:
         print("You lose!")
+        winnings = winnings - bet
+        print("You now have $", winnings, sep="")
 
 
 main()
